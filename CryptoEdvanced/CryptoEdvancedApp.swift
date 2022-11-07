016d6182922cace6f764a168a164f7d52f7e1167
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-07 13:20:45 +0300

    Initial Commit

---
+++ CryptoEdvanced/CryptoEdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoEdvancedApp.swift
+//  CryptoEdvanced
+//
+//  Created by Danil Peregorodiev on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoEdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
