fef3e1a80e2812e550dd3d0954ffde1e440a66b2
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-07 13:23:50 +0300

    Initial commit

--- CryptoEdvanced/ContentView.swift
+++ CryptoEdvanced/ContentView.swift
@@ -11,8 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
