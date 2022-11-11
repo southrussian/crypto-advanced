4519bface7a318eb3797f5440b8083132f1fedef
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-11 16:31:44 +0300

    added coin model struct

--- CryptoEdvanced/Models/CoinModel.swift
+++ CryptoEdvanced/Models/CoinModel.swift
@@ -8,8 +8,27 @@
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
 
 
-import Foundation
 
-class CoinModel {
-    
+struct CoinModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank: Double?
+    let fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H, priceChange24H: Double?
+    let priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H, circulatingSupply: Double?
+    let totalSupply, maxSupply: Double?
+    let ath, athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+    let currentHoldings: Double?
 }
+
+struct SparklineIn7D: Codable {
+    let price: [Double]?
+}
+
