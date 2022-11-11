0c72fbf22d4b5d1065c0124a5e3255dcf15ea18c
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-12 00:15:20 +0300

    Finished coin model

--- CryptoEdvanced/Models/CoinModel.swift
+++ CryptoEdvanced/Models/CoinModel.swift
@@ -54,6 +54,18 @@ struct CoinModel: Identifiable, Codable {
             case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
             case currentHoldings
         }
+    
+    func updateHoldings(amount: Double) -> CoinModel {
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
+    }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
 }
 
 struct SparklineIn7D: Codable {
