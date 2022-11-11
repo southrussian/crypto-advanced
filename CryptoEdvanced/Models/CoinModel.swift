3a415e140b983d9e29472b7127e93e2fa43b13f7
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-11 16:22:43 +0300

    Started coin model

--- CryptoEdvanced/Models/CoinModel.swift
+++ CryptoEdvanced/Models/CoinModel.swift
@@ -5,6 +5,9 @@
 //  Created by Danil Peregorodiev on 11.11.2022.
 //
 
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+
+
 import Foundation
 
 class CoinModel {
