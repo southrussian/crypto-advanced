5c06244da141e9f8e3728bf41c87265897f4a672
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-12 00:07:10 +0300

    added coding keys enum

--- CryptoEdvanced/Models/CoinModel.swift
+++ CryptoEdvanced/Models/CoinModel.swift
@@ -26,9 +26,38 @@ struct CoinModel: Identifiable, Codable {
     let sparklineIn7D: SparklineIn7D?
     let priceChangePercentage24HInCurrency: Double?
     let currentHoldings: Double?
+    
+    enum CodingKeys: String, CodingKey {
+            case id, symbol, name, image
+            case currentPrice = "current_price"
+            case marketCap = "market_cap"
+            case marketCapRank = "market_cap_rank"
+            case fullyDilutedValuation = "fully_diluted_valuation"
+            case totalVolume = "total_volume"
+            case high24H = "high_24h"
+            case low24H = "low_24h"
+            case priceChange24H = "price_change_24h"
+            case priceChangePercentage24H = "price_change_percentage_24h"
+            case marketCapChange24H = "market_cap_change_24h"
+            case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+            case circulatingSupply = "circulating_supply"
+            case totalSupply = "total_supply"
+            case maxSupply = "max_supply"
+            case ath
+            case athChangePercentage = "ath_change_percentage"
+            case athDate = "ath_date"
+            case atl
+            case atlChangePercentage = "atl_change_percentage"
+            case atlDate = "atl_date"
+            case lastUpdated = "last_updated"
+            case sparklineIn7D = "sparkline_in_7d"
+            case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
+            case currentHoldings
+        }
 }
 
 struct SparklineIn7D: Codable {
     let price: [Double]?
 }
+
 
