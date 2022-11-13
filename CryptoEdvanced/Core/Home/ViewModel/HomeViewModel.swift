c4e5d4b711e1174ec3f4532867114a4bca4e41bd
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-13 16:31:00 +0300

    Created DataService and updated ViewModel, initialize getCoins func, download the data, check the data to be valid, put it in a publisher array allCoins, then subscriber allCoins array initializes in ViewVodel

--- CryptoEdvanced/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoEdvanced/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,16 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
     
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DevPreview.instance.coin)
-            self.portfolioCoins.append(DevPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
     
     
--- CryptoEdvanced/Services/CoinDataService.swift
+++ CryptoEdvanced/Services/CoinDataService.swift
@@ -10,7 +10,7 @@ import Combine
 
 class CoinDataService {
     @Published var allCoins: [CoinModel] = []
-    var cancellables = Set<AnyCancellable>()
+    var coinSubscription: AnyCancellable?
     init() {
         getCoin()
     }
@@ -19,7 +19,7 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
         
-        URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
                 guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
@@ -38,7 +38,8 @@ class CoinDataService {
                 }
             } receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
             }
-            .store(in: &cancellables)
+        
     }
 }
