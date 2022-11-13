462ed14ca955f5b89c2db2dc717018ed548bcd9c
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-13 17:38:09 +0300

    Customized error descriptions with enum

--- CryptoEdvanced/Utilities/NetworkingManager.swift
+++ CryptoEdvanced/Utilities/NetworkingManager.swift
@@ -9,17 +9,31 @@ import Foundation
 import Combine
 
 class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "Bad response from URL. \(url)"
+            case .unknown: return "Unknown error occured"
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ try handleURLResponse(output: $0) })
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        throw NetworkingError.badURLResponse(url: url)
         guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badURLResponse(url: url)
         }
         return output.data
     }
