bcddd40369cf903f3ef3ee3c8034ec7d8b41723e
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-20 23:16:20 +0300

    Refactored code, added debounce for delay in 0.5 seconds

--- CryptoEdvanced/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoEdvanced/Core/Home/ViewModel/HomeViewModel.swift
@@ -20,29 +20,43 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins
-            }
-            .store(in: &cancellables)
-        
+//        dataService.$allCoins
+//            .sink { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//            }
+//            .store(in: &cancellables)
+//
         $searchText
             .combineLatest(dataService.$allCoins)
-            .map { (text, startingCoins) -> [CoinModel] in
-                guard !text.isEmpty else {
-                    return startingCoins
-                }
-                let lowercasedText = text.lowercased()
-                return startingCoins.filter { (coin) -> Bool in
-                    return coin.name.lowercased().contains(lowercasedText) ||
-                    coin.symbol.lowercased().contains(lowercasedText) ||
-                    coin.id.lowercased().contains(lowercasedText)
-                }
-            }
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+//            .map { (text, startingCoins) -> [CoinModel] in
+//                guard !text.isEmpty else {
+//                    return startingCoins
+//                }
+//                let lowercasedText = text.lowercased()
+//                return startingCoins.filter { (coin) -> Bool in
+//                    return coin.name.lowercased().contains(lowercasedText) ||
+//                    coin.symbol.lowercased().contains(lowercasedText) ||
+//                    coin.id.lowercased().contains(lowercasedText)
+//                }
+//            }
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
     
     
