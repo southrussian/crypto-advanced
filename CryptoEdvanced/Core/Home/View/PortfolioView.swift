8b4e8385fad09eac9bdbd459627e0ffae38179fb
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-27 00:33:12 +0300

    Added PortfolioView, changed NavBarTitle color

--- CryptoEdvanced/Core/Home/View/PortfolioView.swift
+++ CryptoEdvanced/Core/Home/View/PortfolioView.swift
@@ -11,18 +11,59 @@ struct PortfolioView: View {
         
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
+    @State private var showCheckmark: Bool = false
 
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(SearchedText: $vm.searchText)
-                    ScrollView(.horizontal, showsIndicators: true) {
+                    ScrollView(.horizontal, showsIndicators: false) {
                         LazyHStack(spacing: 10) {
                             ForEach(vm.allCoins) { coin in
-                                Text(coin.symbol.uppercased())
+                                CoinLogoView(coin: coin)
+                                    .frame(width: 75)
+                                    .padding(.vertical, 4)
+                                    .onTapGesture {
+                                        withAnimation(.easeIn) {
+                                            selectedCoin = coin
+                                        }
+                                    }
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(selectedCoin?.id == coin.id ? Color.theme.accent : Color.clear, lineWidth: 1)
+                                    )
                             }
                         }
+                        .padding(.horizontal)
+                    }
+                    if selectedCoin != nil {
+                        VStack(spacing: 20) {
+                            HStack {
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? "")")
+                                Spacer()
+                                Text(selectedCoin?.currentPrice.stringAsNumber() ?? "$0.00")
+                            }
+                            Divider()
+                            HStack {
+                                Text("Amount in your portfolio:")
+                                Spacer()
+                                TextField("Ex: 1.4", text: $quantityText)
+                                    .multilineTextAlignment(.trailing)
+                                    .keyboardType(.decimalPad)
+                            }
+                            Divider()
+                            HStack {
+                                Text("Current Value:")
+                                Spacer()
+                                Text(getCurrentValue().asCurrencyWithTwoDecimals())
+                            }
+                        }
+                        .animation(.none)
+                        .padding()
+                        .font(.headline)
                     }
                 }
             }
@@ -37,6 +78,19 @@ struct PortfolioView: View {
                     }
 
                 }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 10) {
+                        Image(systemName: "checkmark")
+                            .opacity(showCheckmark ? 1.0 : 0.0)
+                        Button {
+                            saveButtonPressed()
+                        } label: {
+                            Text("Save")
+                        }
+                        .opacity((selectedCoin != nil && selectedCoin?.currentHoldings != Double(quantityText)) ? 1.0 : 0.0)
+                    }
+                    .font(.headline)
+                }
             })
         }
     }
@@ -46,5 +100,33 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private func saveButtonPressed() {
+        guard let coin = selectedCoin else { return }
+        
+        withAnimation(.easeIn) {
+            showCheckmark = true
+        }
+        UIApplication.shared.endEditing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            withAnimation(.easeOut) {
+                showCheckmark = false
+            }
+        }
+    }
+    
+    private func removeSelectedCoin() {
+        selectedCoin = nil
+        vm.searchText = ""
     }
 }
--- CryptoEdvanced/CryptoEdvancedApp.swift
+++ CryptoEdvanced/CryptoEdvancedApp.swift
@@ -11,6 +11,13 @@ import SwiftUI
 struct CryptoEdvancedApp: App {
     
     @StateObject private var vm = HomeViewModel()
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
